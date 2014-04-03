3c74bfeb0b1676d9cd03c910ebcf1e8e023606a4
jj4jj <resc@vip.qq.com>
2014-04-03 23:31:03 +0800

    for rename

--- algorithm/RBTree.h
+++ algorithm/RBTree.h
@@ -1,13 +1,28 @@
 #pragma once
 
 
-
+template <class U>
 class RBTree
 {
-
-
-
+public:    
+    struct  Node;
+    enum    NodeColor
+    {
+        NC_RED = 0,
+        NC_BLOCK,
+    };
+    struct  Node
+    {
+        Node* left,*right;
+        NodeColor   nc;         
+        //////////////////
+        U   data;
+    };
+protected:
+    
+public:
+	int		Insert(const U & u);
+	int		Remove(const U & u);
+	U *		Find(const U & u);	
 };
-
-
 
