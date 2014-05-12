992f423f1d98896f1bc73400b85777d0496f69f3
jj4jj <resc@vip.qq.com>
2014-05-12 21:05:09 +0800

    for co

--- utility/Coroutine.h
+++ utility/Coroutine.h
@@ -2,26 +2,74 @@
 
 
 
+
+typedef void	CoroutineFunc(Coroutine co,void* arg);
+
+
+struct	CoroutineData
+{
+	int iID;
+	//context			
+	Buffer	stack;
+	//context
+	ucontext_t	ctx;
+};
+
 class Coroutine
 {
+friend class CoroutineMgr;
+protected:
+	Coroutine(CoroutineData* p):pData(p){}
 public:
-    int     Yield();
-    int     Exit();
-public:
-    int     Start();
+	//return val equal to param is ok . 
+    int     Yield(int ret);
+	int		Switch(Coroutine co,int ret);
+	//return 0 if cf is over
+	//return the param of Yield and Switch
+    int     Start(CoroutineFunc cf);
     int     Resume();
+private:
+	CoroutineData*	pData;
 };
 
 
 
-class CoroutineMgr
+class CoroutineMgr : Singleton<CoroutineMgr>
 {
 public:
-    int         Create();
-    int         Destroy(int iNextCoID);     
-
+    Coroutine	Create();
+	Coroutine*	Find(int id);
+	void		Destroy(Coroutine co);
 private:
-    int         iNextCoID;
+    int				iNextCOID;
+	int				iCosCap;
+	int				iCosCount;
+	CoroutineData*  cos;	
+public:
+	static  Tf(void* p)
+	{
+		for(int i = 0;i < 5;++i)
+		{
+			printf("Tf:%d\n",i);
+			CoroutineMgr::Instance().yield(5-i-1);	
+		}
+	}
+	
+	void	Usage()
+	{
+		
+		Coroutine co = CoroutineMgr::Instance().Create();		
+		int t = co.Start(Tf);
+		for(int i = 0;i < t; ++i)
+		{
+			printf("Usage:%d",i);
+			co.Resume();	
+		}
+		co.Start(Tf);
+		co.Destroy();
+		
+	}
+
 };
 
 
