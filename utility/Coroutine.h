57b531ee6c4473c88feb875c62acc367acff0116
jj4jj <resc@vip.qq.com>
2014-07-07 23:45:24 +0800

    coroutine

--- utility/Coroutine.h
+++ utility/Coroutine.h
@@ -1,7 +1,7 @@
 #pragma once
 
 
-
+#inlcude "ucontext.h"
 
 typedef void	CoroutineFunc(int coid,void* arg);
 
@@ -20,6 +20,12 @@ struct	Coroutine
 		COROUTINE_STATUS_RUNNING = 2,
 		COROUTINE_STATUS_STOP    = 3,
 	};
+    void construct()
+    {
+        iID = 0;
+        bState = 0;
+        from = NULL;        
+    }
 };
 
 
@@ -27,10 +33,114 @@ class CoroutineMgr
 {
 public:
 enum {
-		DEFAULT_STACK_SIZE=8192,	
+		DEFAULT_STACK_SIZE = 8192,	
+        DEFAULT_COROUTINE_NUM = 64,
 	};
 public:
-	Coroutine * Alloc()
+	
+    int	Create(CoroutineFunc f,void * arg)
+	{
+		Coroutine * co = Alloc();
+		if(!co)
+		{
+			return -1;	
+		}
+		co->from = GetCurrent();
+        if(NULL == co->stack.pBuffer)
+        {
+            co->stack.Create(DEFAULT_STACK_SIZE);
+        }
+        getcontext(&(co->ctx));
+        co->ctx.uc_link = 0;
+        co->ctx.uc_stack.ss_sp = co->stack.pBuffer;
+        co->ctx.uc_stack.ss_size = co->stack.iCap;
+        makecontext(&co->ctx, (void(*)(void))&f,2,(uint32_t)co->iID,arg);
+        co->bState = Coroutine::COROUTINE_STATUS_SUSPEND;        
+		return co->iID;
+	}
+	int	Resume(int coid)
+	{
+		Coroutine * co = Find(coid);
+		if(!co || co->bState == Coroutine::COROUTINE_STATUS_STOP )
+		{
+			return -1;	
+		}
+		Coroutine * cur = GetCurrent();
+		co->from = cur;
+		co->bState = Coroutine:: COROUTINE_STATUS_RUNNING
+		//make ctx
+        SetCurrent(co);
+        swapcontext(&(co->ctx),&(cur->ctx));
+		return 0;
+	}
+	int	Yield(int coid)
+	{
+        Coroutine * cur = GetCurrent();
+        if(!cur)
+        {
+            return -1;
+        }
+		Coroutine * co = Find(coid);
+		if(0 == coid)
+		{
+			//switch to prev
+			co = cur->prev;
+		}
+        if(!co)
+        {
+            return -1;
+        }
+        cur->bState = Coroutine::COROUTINE_STATUS_SUSPEND;
+		co->bState = Coroutine::COROUTINE_STATUS_RUNNING;
+        SetCurrent(co);
+        swapcontext(&(co->ctx),&(cur->ctx));
+        return 0;
+	}
+	int	GetStatus(int coid)
+	{
+		Coroutine * co = Find(coid);
+		if(!co)
+		{
+			
+			return -1;
+		}
+		return co->bState;
+	}
+	Coroutine*	Find(int id)
+	{
+		if(id >= iCosCap)
+		{
+			return NULL;	
+		}
+		Coroutine * co =  cos[id];
+		if(co.iID == id )
+		{
+			return &co;	
+		}
+		return NULL;	
+	}
+    void    BackTrace()
+    {
+        Coroutine* co = GetCurrent();
+        int i = 0;
+        while(co)
+        {
+            printf("coroutine %0002d:[id=%0002d,addr=0x%08x]",i,co->iID,co):
+            co = co->from;
+            ++i;
+        }
+    }
+
+private:
+    Coroutine*  GetCurrent()
+    {
+        return current;
+    }
+    void        SetCurrent(Coroutine* co)
+    {
+        current = co;
+    }
+    Coroutine * Alloc()
 	{
 		int iID = 1;
 		while(iID < iCosCount &&
@@ -38,18 +148,22 @@ public:
 		{
 			iID++;
 		}
-
-
 		if(iID == iCosCount )//iCosCount >= iCosCap)
 		{
 			if(iCosCount >= iCosCap)
 			{
-				cos = realloc(cos,2*iCosCap);
+                assert(iCosCount == iCosCap);
+				cos = realloc(cos,2*iCosCap*sizeof(Coroutine));
 				if(!cos)
 				{
 					//no mem more
 					return NULL;	
 				}
+
+                for(int i = iCosCap;i < 2*iCosCap; ++i)
+                {
+                    cos[i].construct();
+                }
 				iCosCap *= 2;
 			}
 			iCosCount++;
@@ -62,72 +176,34 @@ public:
 		co.iID = iID;
 		return &co;
 	}
-    int	Create(CoroutineFunc f,void * arg)
-	{
-		Coroutine * co = Alloc();
-		if(!co)
+    int Init()
+    {
+        iCosCap = DEFAULT_COROUTINE_NUM;
+        iCosCount = 1;
+		cos = malloc(iCosCap*sizeof(Coroutine));
+		if(!cos)
 		{
-			return -1;	
-		}
-		co->iID = iNextId;
-		co->stack.Create(DEFAULT_STACK_SIZE);
-		co->from = GetCurrent();
-		//co.ctx = 
-		//set co.ctx.start = f.
-		//set co.ctx.
-		return co->iID;
-	}
-	int	Resume(int coid)
-	{
-		Coroutine * co = Find(coid);
-		if(!co)
-		{
-			return -1;	
-		}
-		Coroutine * cur = GetCurrent();
-		co->from = cur;
-		co->bState = Coroutine:: COROUTINE_STATUS_RUNNING
-		//make ctx
-		
-		return 0;
-	}
-	int	Yield(int coid)
-	{
-		Coroutine * co = Find(coid);
-		if(0 == coid)
-		{
-			//switch to prev
-		}
-		co->bState = Coroutine::COROUTINE_STATUS_SUSPEND;
-	}
-	int	GetStatus(int coid)
-	{
-		Coroutine * co = Find(coid);
-		if(!co)
-		{
-			
+			//no mem more
 			return -1;
-		}
-		return co->bState;
-	}
+		}        
+        for(int i = 0;i < iCosCap; ++i)
+        {
+            cos[i].construct();
+        }
+        Coroutine & co = cos[0];
+        co.bState = Coroutine::COROUTINE_STATUS_RUNNING;
+        co.from = NULL;
+        co.iID = 0;        
+        current = &co;
+        getcontext(&(co.ctx));//just for initializing
+        return 0;
+    }
 
-	Coroutine*	Find(int id)
-	{
-		if(id >= iCosCap)
-		{
-			return NULL;	
-		}
-		Coroutine * co =  cos[id];
-		if(co.iID == id)
-		{
-			return &co;	
-		}
-		return NULL;	
-	}
 private:
 	int				iCosCap;
 	int				iCosCount;
 	Coroutine	*	cos;	
+    Coroutine   *   current;
 public:
 	static void	f1(int,void* p)
 	{
