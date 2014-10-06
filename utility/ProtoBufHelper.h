45802c932859e809d6f68a8c2217484c9735739d
jj4jj <resc@vip.qq.com>
2014-10-07 01:26:02 +0800

    proto buffer helper add

--- utility/ProtoBufHelper.h
+++ utility/ProtoBufHelper.h
@@ -1,15 +1,39 @@
 #pragma once
+#include "base/stdinc.h"
 #include "base/Buffer.h"
+
+#include "google/protobuf/compiler/code_generator.h"
+#include "google/protobuf/compiler/cpp/cpp_generator.h"
+#include "google/protobuf/compiler/python/python_generator.h"
+
+//#include "google/protobuf/compiler/plugin.pb.h"
+#include "google/protobuf/compiler/plugin.h"
+
+#include "google/protobuf/descriptor.h"
+#include "google/protobuf/message.h"
+#include "google/protobuf/compiler/parser.h"
+#include "google/protobuf/compiler/importer.h"
+
 
 class ProtoBufHelper
 {
-public:    
-    int Pack(const char* pszMetaName,const Buffer & objData,Buffer & buffer);
-    int UnPack(const char* pszMetaName,const Buffer & buffer,Buffer objData);    
-    //todo list meta sub attr
-    //get descripter and sthm
-};
+public:
+    int     Init(const char* pszMetaFileName);
+    int     GetPackSize(void* obj);
+    int     Pack(void* obj,Buffer & buffer);
+    int     UnPack(const char* pszMetaName,Buffer & buffer,void * * ppObj);
+    void    Visual(void* pObj,string & s);
 
+public:
+    template<class T>
+    void    FreeObj(T* pObj){delete pObj;}
+
+private:
+    google::protobuf::DescriptorPool*       m_pDescriptorPool;
+    google::protobuf::MessageFactory*       m_pObjFactory;
+    
+    
+};
 
 
 
